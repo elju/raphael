edc71550cf575b36395a263d7c96f6f709e4d3d9
elju <moresilenter@gmail.com>
2013-07-24 18:45:29 -0400

    almost there

--- example.js
+++ example.js
@@ -1,43 +1,48 @@
 $(function() {
+    var SX = window.innerWidth; //screenX
+    var SY = window.innerHeight; //screenY
     var paper = Raphael(0, 0, window.innerWidth, window.innerHeight);
     var circle = paper.circle(50,50,20);
     circle.attr({fill:"#000"});
-    var CHANGEX = 30;
-    var CHANGEY = 20;
-    var SX = window.innerWidth; //screenX
-    var SY = window.innerHeight; //screenY
-    circle.update = function () {
-        console.log("yo");
-        var dx = CHANGEX;
-        var dy = CHANGEY;
+    this.CHANGEX = Math.floor(Math.random() * 60);
+    this.CHANGEY = Math.floor(Math.random() * 40);
+    var num;
+    var intervals = [];
+    var circles = paper.set();
+    circles.update = function () {
+        var dx = this.CHANGEX;
+        var dy = this.CHANGEY;
         var x = this.attr("cx");
         var y = this.attr("cy");
         var X, Y;
         if (x + dx > SX) {
             X = SX - (dx - (SX - x)); // bounce off the wall in the x direction;
-            CHANGEX = -CHANGEX;
+            this.CHANGEX = -this.CHANGEX;
+            this.animate({fill: makeColor()}, 1000);
         } else if (x + dx < 0) {
             X = -dx - x;
-            CHANGEX = -CHANGEX;
+            this.CHANGEX = -this.CHANGEX; // change to moving opposite x direction
+            this.animate({fill: makeColor()}, 1000);
         } else {
             X = x + dx;
         }
         if (y + dy > SY) {
             Y = SY - (dy - (SY - y)); // bounce off the wall in the x direction;
-            CHANGEY = -CHANGEY;
+            this.CHANGEY = -this.CHANGEY;
+            this.animate({fill: makeColor()}, 1000);
         } else if (y + dy < 0) {
             Y = -dy - y;
-            CHANGEY = -CHANGEY;
+            this.CHANGEY = -this.CHANGEY; // change to moving opposite y direction;
+            this.animate({fill: makeColor()}, 1000);
         } else {
             Y = y + dy;
         }
         this.animate({cx: X, cy: Y}, 100);
-        console.log(this);
     };
     var path = paper.path("M50,50L450,50").attr({"stroke-width": 8, stroke: "#000", "stroke-linecap": "round"});
     var path2 = paper.path("M50,50L450,50").attr({"stroke-width": 6, stroke: "#fff", "stroke-linecap": "round"});
-    var dragger = paper.circle(200,50,12).attr({stroke:"#000",fill:"#ccc"});
-    var texty = paper.text(200,50,"4");
+    var dragger = paper.circle(90,50,12).attr({stroke:"#000",fill:"#ccc"});
+    var texty = paper.text(200,50,"4").attr({unselectable: "on"});
 
     function up() {
         this.dx = this.dy = 0;
@@ -51,8 +56,24 @@ $(function() {
         temp = temp > 70 ? temp : 70;
         temp = temp < 430 ? temp : 430;
         var X = temp;
+        var tempCircle;
         this.attr({cx: X});
-        var num = Math.floor(X / 50);
+        var tempNum = Math.floor(X / 50);
+        num = num || 1;
+        if (tempNum !== num) {
+            if (tempNum > num) {
+                tempCircle = paper.circle(Math.floor(Math.random() * SX), Math.floor(Math.random() * SY), 30).attr({fill:"#0F0"});
+                tempCircle.CHANGEX = Math.floor(Math.random() * 80) + 1;
+                tempCircle.CHANGEY = Math.floor(Math.random() * 80) + 1;
+                tempCircle.attr({"stroke-width": 0});
+                circles.push(tempCircle);
+                intervals.push(window.setInterval(circles.update.bind(tempCircle), 100));
+            } else {
+                circles.pop().remove();
+                window.clearInterval(intervals.pop());
+            }
+        }
+        num = tempNum;
         texty.attr({x:X,y:50,text:num});
     };
 
@@ -66,6 +87,16 @@ $(function() {
         this.dy = dy;
     }
     dragger.drag(move, up);
+    texty.drag(move.bind(dragger), up.bind(dragger));
 
-    window.setInterval(circle.update.bind(circle), 100);
+    intervals.push(window.setInterval(circles.update.bind(circle), 100));
+
+    function makeColor() {
+        var thing = "#";
+        for(var i = 0; i < 3; i++) {
+            thing += Math.floor(Math.random()*16).toString(16);
+        }
+        return thing;
+    }
+
 });
